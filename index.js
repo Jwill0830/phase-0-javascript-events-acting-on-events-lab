cc27e97319c9169a7f60df3a67e789499f3dc1bf
Jack Williams <jackmwilliams35@gmail.com>
2023-09-01 14:57:18 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+let dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  // Get the current "left" position of the dodger element
+  let left = dodger.style.left.replace("px", "");
+  let leftInteger = parseInt(left, 10);
+
+  // Move the dodger to the left
+  if (leftInteger > 0) {
+    dodger.style.left = `${leftInteger - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  // Get the current "left" position of the dodger element
+  let left = dodger.style.left.replace("px", "");
+  let leftInteger = parseInt(left, 10);
+
+  // Move the dodger to the right
+  if (leftInteger < 360) {
+    dodger.style.left = `${leftInteger + 1}px`;
+  }
+}
